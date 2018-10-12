dabaf54d8977f42ad143ac1cedfb78475993a668
christopher.miller <christopher.miller@MU-C1-14.mullen.pvt.k12.co.us>
2018-10-12 15:02:18 -0600

    To Do: First, rando num generator, then program them being able to place the numbers in the grid

--- Two Dimensional Project/src/Display.java
+++ Two Dimensional Project/src/Display.java
@@ -6,7 +6,6 @@ public class Display
 			{
 				fillArray();
 				display();
-
 			}
 
 	
@@ -20,7 +19,7 @@ public class Display
 						for(int col = 0; col < 7; col++)
 							{
 								
-								System.out.print(board[row][col] + " ");
+								board[row][col] = " ";
 								
 							}
 					}
@@ -34,31 +33,31 @@ public class Display
 			System.out.println("        1      2      3      4      5      6      7   ");
 			System.out.println("    --------------------------------------------------");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   A|   " + board [0][0] + "    |    " + board [0][1] + "  |  "  +    "|      |      |      |      |");
+			System.out.println("   A|   " + board [0][0] + "  |  " + board [0][1] + "   | "  + board [0] [2] +   "    | "   + board [0][3] + "    | " + board [0][4] + "    |  " + board [0][5] + "   | " + board[0][6] +  "    |"  );
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   B|      |      |      |      |      |      |      |");
+			System.out.println("   B|   " + board [1][0] + "  |  " + board [1][1] + "   | "  + board [1][2] +  "    | " + board [1][3] + "    |" + board [1][4] + "     |" + board [1][5] + "     | " + board [1][6] + "    |");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   C|      |      |      |      |      |      |      |");
+			System.out.println("   C|   " + board [2][0] + "  |  " + board [2][1] + "   | "  + board [2][2] +  "    | " + board [2][3] + "    |" + board [2][4] + "     |" + board [2][5] + "     | " + board [2][6] + "    |" );
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   D|      |      |      |      |      |      |      |");
+			System.out.println("   D|   " + board [3][0] + "  |  " + board [3][1] + "   | "  + board [3][2] +  "    | " + board [3][3] + "    |" + board [3][4] + "     |" + board [3][5] + "     | " + board [3][6] + "    |"  );
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   E|      |      |      |      |      |      |      |");
+			System.out.println("   E|   " + board [4][0] + "  |  " + board [4][1] + "   | "  + board [4][2] +  "    | " + board [4][3] + "    |" + board [4][4] + "     |" + board [4][5] + "     | " + board [4][6] + "    |" );
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   F|      |      |      |      |      |      |      |");
+			System.out.println("   F|   " + board [5][0] + "  |  " + board [5][1] + "   | "  + board [5][2] +  "    | " + board [5][3] + "    |" + board [5][4] + "     |" + board [5][5] + "     | " + board [5][6] + "    |" );
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("    |------------------------------------------------|");
+			System.out.println("    |------+------+------+------+------+------+------|");
 			System.out.println("    |      |      |      |      |      |      |      |");
-			System.out.println("   G|      |      |      |      |      |      |      |");
+			System.out.println("   G|   " + board [6][0] + "  |  " + board [6][1] + "   | "  + board [6][2] +  "    | " + board [6][3] + "    |" + board [6][4] + "     |" + board [6][5] + "     | " + board [6][6] + "    |" );
 			System.out.println("    |      |      |      |      |      |      |      |");
 			System.out.println("    --------------------------------------------------");
 		
